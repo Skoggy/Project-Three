354ab8948fce0e904c2a01383b89fc79b7037c6d
Chris Skogstad <chris.skogstad1@gmail.com>
2021-01-11 21:42:54 +1100

    react and server working together rough

--- server.js
+++ server.js
@@ -1,23 +1,43 @@
+// const express = require("express");
+// const routes = require("./routes");
+
+// // Sets up the Express App
+// // =============================================================
+// const app = express();
+// const PORT = process.env.PORT || 8080;
+
+// // Requiring our models for syncing
+// const db = require("./models");
+
+// app.use(routes);
+// // Sets up the Express app to handle data parsing
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// // Static directory
+// app.use(express.static("public"));
+
+
+// db.sequelize.sync({ force: true }).then(function () {
+//     app.listen(PORT, function () {
+//         console.log("App listening on PORT " + PORT);
+//     });
+// });
+
+
+
+const path = require("path");
 const express = require("express");
-
-// Sets up the Express App
-// =============================================================
-const app = express();
-const PORT = process.env.PORT || 8080;
-
-// Requiring our models for syncing
-const db = require("./models");
-
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// Static directory
-app.use(express.static("public"));
+const app = express(); // create express app
 
 
-db.sequelize.sync({ force: true }).then(function () {
-    app.listen(PORT, function () {
-        console.log("App listening on PORT " + PORT);
-    });
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+// start express server on port 5000
+app.listen(7500, () => {
+    console.log("server started on port 7500");
 });
