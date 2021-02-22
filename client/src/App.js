b2c05b92976527b9e4a5e0383e7b42b58e45785b
Chris Skogstad <chris.skogstad1@gmail.com>
2021-02-22 22:58:43 +1100

    add redux for use with user state

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
-import { React } from 'react';
+import { React, useState, useEffect } from 'react';
+
+import { connect } from "react-redux";
+
+import { addUser } from "./redux/actions";
+import { getUserBaseOnToken } from "./API/auth"
+
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import GlobalStyles from './styles/GlobalStyles';
 import Header from './components/Header';
 import Typography from './styles/Typography';
 import Footer from './components/Footer'
 import { LoginPage } from './pages/login'
-import ProtectedRoute from './components/ProtectedRoute'
-import { UserProvider } from './utils/UserContext';
+import { ProtectedRoute } from './components/routes/ProtectedRoute';
+import { PublicRoute } from './components/routes/PublicRoute'
 import { frontPage } from './pages/frontPage';
 import { SelectStockPage } from './pages/selectStockPage';
 import { RegisterPage } from './pages/registerPage';
@@ -16,59 +22,68 @@ import styled from 'styled-components'
 
 
 
+const Flex = styled.div`
 
-function App() {
+display: flex;
+flex-flow: column;
+height:98vh;
+max-height: 98vh;
 
-  const Flex = styled.div`
+.header {
+  flex: 0 1 auto;
+}
+.content {
+  flex: 1 1 auto;
+}
+.footer {
+  flex: 0 1 auto;
+}
+`
 
-        display: flex;
-        flex-flow: column;
-        height:98vh;
-        max-height: 98vh;
+function App({ dispatch }) {
+  const [loading, setLoading] = useState(true);
 
-        .header {
-          flex: 0 1 auto;
-        }
-        .content {
-          flex: 1 1 auto;
-        }
-        .footer {
-          flex: 0 1 auto;
-        }
-  `
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token)
+      getUserBaseOnToken(token).then((res) => {
+        dispatch(addUser(res.data.data));
+        setLoading(false);
+      });
+    else setLoading(false);
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
 
 
   return (
     <Flex>
 
-      <UserProvider>
-        <GlobalStyles />
-        <Typography />
-        <Router>
-          <div className="header">
-            <Header />
-          </div>
-          <div className="content">
 
-            <Switch>
-              <Route exact path='/login' component={LoginPage} />
-              <Route exact path='/stocktype' component={SelectStockPage} />
-              <Route exact path='/takeitem' component={takeItemPage} />
-              <Route exact path='/register' component={RegisterPage} />
-              <Route exact path='/' component={frontPage} />
-              <ProtectedRoute exact path='/admin' component={Admin} />
-            </Switch>
-
-          </div>
-        </Router>
-        <div className="footer">
-          <Footer />
+      <GlobalStyles />
+      <Typography />
+      <Router>
+        <div className="header">
+          <Header />
         </div>
-      </UserProvider>
+        <div className="content">
+          <Switch>
+            <PublicRoute exact path='/login' component={LoginPage} />
+            <PublicRoute exact path='/stocktype' component={SelectStockPage} />
+            <PublicRoute exact path='/takeitem' component={takeItemPage} />
+            <PublicRoute exact path='/register' component={RegisterPage} />
+            <PublicRoute exact path='/' component={frontPage} />
+            <ProtectedRoute exact path='/admin' component={Admin} />
+          </Switch>
 
+        </div>
+      </Router>
+      <div className="footer">
+        <Footer />
+      </div>
     </Flex >
 
   );
 }
 
-export default App;
+export default connect()(App);
