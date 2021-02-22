3485c93c9870bfa586355a39cc6f705f48d50516
Chris Skogstad <chris.skogstad1@gmail.com>
2021-02-22 23:00:30 +1100

    logout button now clears JWT onclick to properly log out user

--- client/src/components/LogoutButton.js
+++ client/src/components/LogoutButton.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import { withRouter } from "react-router-dom"
+
+import { connect } from 'react-redux';
 import styled from 'styled-components';
+
+import { LOGOUT_ACTION } from '../redux/actions';
+
+function mapStateToProps(state) {
+    return {
+        user: state.user,
+    }
+}
+
 
 
 const LogoutButtonStyles = styled.button`
 
 `
+export const LogoutButton = connect(mapStateToProps)(function ({ dispatch, user }) {
 
-
-export const LogoutButton = withRouter((props) => {
-
-
-
-    const logout = (e) => {
-        e.preventDefault();
-        props.history.push("/")
-    }
     return (
-
-        <LogoutButtonStyles onClick={logout}>Logout</LogoutButtonStyles>
-
+        <div>
+            <button
+                onClick={() => {
+                    dispatch({ type: LOGOUT_ACTION });
+                    localStorage.setItem("token", null);
+                }}
+            >Logout</button>
+        </div>
     )
-})
+});
