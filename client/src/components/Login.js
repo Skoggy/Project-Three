418a91a5a91e86a99d30af9383b730d93b4cd1a2
Chris Skogstad <chris.skogstad1@gmail.com>
2021-02-23 23:22:54 +1100

    login component

---
+++ client/src/components/Login.js
@@ -0,0 +1,121 @@
+
+import { React, useState } from 'react';
+import styled from 'styled-components';
+
+import bg from '../assets/images/background.jpg';
+import { withRouter } from "react-router-dom"
+
+import { connect } from 'react-redux';
+
+import { login } from "../redux/actions";
+import { loginAPI } from '../API/auth';
+
+const DEFAULT_STATE = {
+    email: "",
+    password: "",
+};
+
+const Container = styled.div`
+    display:flex;
+    justify-content: center;
+    flex-direction:row;
+    padding-top:2rem;
+   
+    /* height:68vh; */
+    @media (max-width: 500px ) {
+        margin-top: 8rem;
+        flex-direction:row;
+    }
+`
+const OtherHalf = styled.div`
+min-height:70vh;
+border-left: 2px ridge black;
+margin-left: 2rem;
+background-image: url(${bg});
+width:70vh;
+  background-size: cover;
+  @media (max-width: 500px) {
+      display:none;
+  }
+`
+
+const InputStyles = styled.div`
+display:flex;
+ /* margin-top: 14rem; */
+width:70vh;
+flex-direction:column;
+border: 5px solid black;
+align-items: center; 
+justify-content: center;
+font-size:5rem;
+.input {
+    height:5rem;
+}
+    `
+const LoginForm = ({ loginUser }) => {
+
+    const [state, setState] = useState(DEFAULT_STATE);
+    const [loading, setLoading] = useState(false);
+
+    function onInputChange({ target: { name, value } }) {
+        setState({ ...state, [name]: value })
+    }
+
+
+    return (
+        <Container>
+
+            <InputStyles>
+                <h2>LOGIN</h2>
+                <form
+                    onSubmit={async (e) => {
+                        e.preventDefault();
+                        try {
+                            const { email, password } = state;
+                            if (!email || !password) return;
+
+                            setLoading(true);
+                            const { data } = await loginAPI({ email, password });
+                            localStorage.setItem("token", data.data.token);
+                            setLoading(false);
+
+                            loginUser(data.data);
+                        } catch (err) {
+                            setLoading(false);
+                            console.log(err)
+                        }
+                    }}
+                >
+                    <input
+                        type="text"
+                        placeholder="email"
+                        name="email"
+                        required
+                        onChange={onInputChange}
+                        value={state.email}
+                    />
+                    <input
+                        type="password"
+                        placeholder="password"
+                        name="password"
+                        required
+                        onChange={onInputChange}
+                        value={state.password}
+                    />
+                    <button type="submit" disabled={loading}>
+                        Login
+                </button>
+                </form>
+                <button onClick={() => window.location.href = '/register'}>Register New User</button>
+            </InputStyles>
+            <OtherHalf></OtherHalf>
+        </Container>
+    )
+}
+function mapDispatchToProps(dispatch) {
+    return {
+        loginUser: (data) => dispatch(login(data)),
+    }
+}
+
+export const Login = connect(() => ({}), mapDispatchToProps)(LoginForm);
