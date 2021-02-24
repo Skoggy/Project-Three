7ff225a88b9413b8db2b3af20e0e080b54f1e252
Chris Skogstad <chris.skogstad1@gmail.com>
2021-02-25 05:36:44 +1100

    fix header style

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,12 +6,15 @@ const HeaderStyles = styled.div`
  display:grid;
  grid-template-columns: 1fr 10fr;
 background-color: var(--red);
+align-items:center;
+justify-items:center;
+padding-right:100px;
 height: 7rem;
-text-align: center;
+/* text-align: center; */
 font-size: 5rem;
 color:white;
 border-radius:8px;
-
+margin-right: 5px;
 `
 
 export default function Header() {
